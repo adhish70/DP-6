38023c32a10ed6b904f5a752b2eb58ebfcc7c8d6
Adhish Shrivastava <adhish70@gmail.com>
2022-01-17 11:59:33 -0500

    Initial Commit

---
+++ Problem-169 Longest Palindromic Substring.py
@@ -0,0 +1,11 @@
+# 5. Longest Palindromic Substring
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:   
+    def longestPalindrome(self, s: str) -> str:
+        pass
---
+++ Problem-170 Ugly Number II.py
@@ -0,0 +1,11 @@
+# 264. Ugly Number II
+# https://leetcode.com/problems/ugly-number-ii/
+
+# Logic: 
+
+# Time Complexity: 
+# Space Complexity: 
+
+class Solution:
+    def nthUglyNumber(self, n: int) -> int:
+        pass
