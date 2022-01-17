b123ab79fb4bd1382876c2a94703aa1feadafc08
Adhish Shrivastava <adhish70@gmail.com>
2022-01-17 15:04:44 -0500

    Problem 169 done

--- Problem-169 Longest Palindromic Substring.py
+++ Problem-169 Longest Palindromic Substring.py
@@ -1,11 +1,28 @@
 # 5. Longest Palindromic Substring
 # https://leetcode.com/problems/longest-palindromic-substring/
 
-# Logic: 
+# Logic: For each index, we start an odd length and an even length palindrome search.
 
-# Time Complexity: 
-# Space Complexity: 
+# Time Complexity: O(n*n)
+# Space Complexity: O(1)
 
-class Solution:   
+class Solution:
+    def _helper(self, s, l, r):
+        while l >= 0 and r < len(s) and s[l] == s[r]:
+            l -= 1
+            r += 1
+        
+        return s[l+1:r]
+    
     def longestPalindrome(self, s: str) -> str:
-        pass
+        res = ''
+        
+        for i in range(len(s)):
+            odd_len = self._helper(s, i, i) # We have to start expanding from the same element when odd length input
+            even_len = self._helper(s, i, i+1)
+            
+            temp = odd_len if len(odd_len) > len(even_len) else even_len
+            if len(temp) > len(res):
+                res = temp
+        
+        return res
